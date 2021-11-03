8c51ab851cd9b55b46064e37969556f983342005
Mario Julian Baron Rueda <mariobaron@gmail.com>
2021-11-03 01:46:01 -0500

    Escucha de eventos al crear tarea

--- infraestructure/receivers/subs-events/src/main/java/com/sofka/ejecuciontareas/reactive/EventsSubscriptionsConfig.java
+++ infraestructure/receivers/subs-events/src/main/java/com/sofka/ejecuciontareas/reactive/EventsSubscriptionsConfig.java
@@ -3,6 +3,8 @@ package com.sofka.ejecuciontareas.reactive;
 
 import com.sofka.ejecuciontareas.JobController;
 import com.sofka.ejecuciontareas.common.event.JobCreatedEvent;
+import com.sofka.ejecuciontareas.common.event.notification.CanonicalNotification;
+import com.sofka.ejecuciontareas.domain.canonical.JobCanonical;
 import com.sofka.ejecuciontareas.reactive.dto.message.EventMessage;
 import lombok.RequiredArgsConstructor;
 import org.reactivecommons.async.api.HandlerRegistry;
@@ -24,12 +26,16 @@ public class EventsSubscriptionsConfig {
     @Bean
     public HandlerRegistry eventSubscriptions() {
 
-        /*controller.processCreateJobEvent(JobCanonical.builder()
-                        .id("d1f4315d-9e0d-425e-b9c7-a93fa4edef1e")
-                        .cronRegExp("0 * * * * ?")
-                        .email("mariobaron@gmail.com")
-                        .timeZone("UTC−05:00")
-                        .url("www.facebook.com")
+        /*controller.processCreateJobEvent(JobCreatedEvent.builder()
+                        .canonical(CanonicalNotification.builder()
+                                .data(JobCanonical.builder()
+                                        .id("d1f4315d-9e0d-425e-b9c7-a93fa4edef1e")
+                                        .cronRegExp("0 * * * * ?")
+                                        .email("mariobaron@gmail.com")
+                                        .timeZone("UTC−05:00")
+                                        .url("www.facebook.com")
+                                        .build())
+                                .build())
                 .build());*/
 
         return register().listenEvent(JobCreatedEvent.EVENT_NAME,
